7644b5e5b38d1d13662d32cfc581722b79bc8186
vasanth <vasanthhari321@gmail.com>
2022-11-01 00:08:31 +0530

    first commit

---
+++ CRUDUsingMVCwithAdoDotNet/Controllers/EmployeeController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDUsingMVC.Models;
+using CRUDUsingMVC.Repository;
+namespace CRUDUsingMVC.Controllers
+{
+   
+    public class EmployeeController : Controller
+    {
+      
+       
+        // GET: Employee/GetAllEmpDetails
+      
+        public ActionResult GetAllEmpDetails()
+        {
+          
+            EmpRepository EmpRepo = new EmpRepository();
+            ModelState.Clear();
+            return View(EmpRepo.GetAllEmployees());
+        }
+
+
+        // GET: Employee/AddEmployee
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        // POST: Employee/AddEmployee
+        [HttpPost]
+        public ActionResult AddEmployee(EmpModel Emp)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    EmpRepository EmpRepo = new EmpRepository();
+
+                    if (EmpRepo.AddEmployee(Emp))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+              
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/EditEmpDetails/5
+        public ActionResult EditEmpDetails(int id)
+        {
+            EmpRepository EmpRepo = new EmpRepository();
+
+          
+
+            return View(EmpRepo.GetAllEmployees().Find(Emp => Emp.Empid == id));
+
+        }
+
+        // POST: Employee/EditEmpDetails/5
+        [HttpPost]
+   
+        public ActionResult EditEmpDetails(int id,EmpModel obj)
+        {
+            try
+            {
+                    EmpRepository EmpRepo = new EmpRepository();
+                  
+                    EmpRepo.UpdateEmployee(obj);
+               
+
+
+
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/DeleteEmp/5
+        public ActionResult DeleteEmp(int id)
+        {
+            try
+            {
+                EmpRepository EmpRepo = new EmpRepository();
+                if (EmpRepo.DeleteEmployee(id))
+                {
+                    ViewBag.AlertMsg = "Employee details deleted successfully";
+
+                }
+                return RedirectToAction("GetAllEmpDetails");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+   
+    }
+}
